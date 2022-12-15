c54bab92a5da66ed12e049944c7ef9d685b6c94a
auliaarifin <auliaarifin89@gmail.com>
2022-12-15 14:09:38 +0700

    membuat QR ke browser

--- app.js
+++ app.js
@@ -1,35 +1,59 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
- 
-const qrcode = require('qrcode-terminal');
+const express = require('express');
+const socketIO = require('socket.io');
+const qrcode = require('qrcode');
+const http = require('http');
+const { url } = require('inspector');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server)
 
 const client = new Client({
-    authStrategy: new LocalAuth()
+  authStrategy: new LocalAuth()
 });
 
-client.on('qr', (qr) => {
-  // Generate and scan this code with your phone
-  console.log('QR RECEIVED', qr);
-  qrcode.generate(qr);
-});
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+})
 
-client.on('authenticated', () =>{
+
+client.on('authenticated', () => {
   console.log('AUTHENTICATED');
-});
-
-client.on('ready', () => {
-  console.log('Client is ready!');
 });
 
 
 client.on('message', msg => {
   if (msg.body == '!ping') {
     msg.reply('Hey There !! Whatsapp Bot under Developmnet');
-  } else if (msg.body == 'ul'){
+  } else if (msg.body == 'ul') {
     msg.reply("Hey There !! Whatsapp Bot under Developmnet");
-  } else if (msg.body == 'Lek'){
+  } else if (msg.body == 'Lek') {
     msg.reply("Hey There !! Whatsapp Bot under Developmnet");
   }
 });
 
 client.initialize();
+
+//socket IO
+
+io.on('connection', function(socket){
+  socket.emit('message', 'Connecting....')
+
+  //barcode
+  client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.toDataURL(qr,(err, url) => {
+      socket.emit('qr', url);
+      socket.emit('message', 'QR Code Recived, silahkan scan')
+    });
+  });
+
+  client.on('ready',()=>{
+    socket.emit('message', 'Whatsapp is ready!')
+  })
+  
+})
+server.listen(3000, function () {
+  console.log('Running on port 3000')
+}) 
