64cce12e95790b18a01828672a929a87b9c2edf5
auliaarifin <auliaarifin89@gmail.com>
2022-12-19 15:38:13 +0700

    unsolved cron job

--- app.js
+++ app.js
@@ -4,13 +4,33 @@ const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
 const cron = require('node-cron')
+const {body, validationResult } = require('express-validator');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server)
+const io = socketIO(server);
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true
+}));
 
 
 const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      // '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
   authStrategy: new LocalAuth()
 });
 
@@ -26,6 +46,7 @@ client.on('authenticated', () => {
 });
 
 const db = require('./src/config/db');
+const { response } = require('express');
 
 client.on('message', async msg => {
   const keyword = msg.body.toLowerCase();
@@ -35,9 +56,41 @@ client.on('message', async msg => {
     msg.reply(replyMessage);
 
   } else if (msg.body == 'Tes') {
-    msg.reply("Hey There !! Whatsapp Bot under Developmnet");
+    msg.sendMessage(msg.from, "Hey There !! Whatsapp Bot under Developmnet");
   }
 });
+
+// //Send Message
+// app.post('/send-message', [
+//   body('number').notEmpty,
+//   body('message').notEmpty,
+// ], (req, res) =>{
+//   const error = validationResult(req).formatWith(({}) => {
+//     return msg;
+//   });
+
+//   if (!error.isEmpty()){
+//     return res.status(422).json({
+//       status: false,
+//       message: errors.mapped()
+//     })
+//   }
+
+//   const number = req.body.number;
+//   const message = req.body.message;
+
+//   client.sendMessage(number, message).then(response => {
+//     res.status(200).json({
+//       status: true,
+//       response: response
+//     });
+//   }).catch(err => {
+//     res.status(500).json({
+//       status: false,
+//       response: err,
+//     });
+//   });
+// });
 
 client.initialize();
 
