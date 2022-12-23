ec791644015fb4b80d265046064e0fb17a4e7ec5
auliaarifin <auliaarifin89@gmail.com>
2022-12-23 15:47:16 +0700

    add post and media message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const express = require('express');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
@@ -9,6 +9,7 @@ const {body, validationResult } = require('express-validator');
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
+const kontak = require("./contact.json")
 
 app.use(express.json());
 app.use(express.urlencoded({
@@ -17,20 +18,20 @@ app.use(express.urlencoded({
 
 
 const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      // '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
-    ],
-  },
+  // restartOnAuthFail: true,
+  // puppeteer: {
+  //   headless: true,
+  //   args: [
+  //     '--no-sandbox',
+  //     '--disable-setuid-sandbox',
+  //     '--disable-dev-shm-usage',
+  //     '--disable-accelerated-2d-canvas',
+  //     '--no-first-run',
+  //     '--no-zygote',
+  //     // '--single-process', // <- this one doesn't works in Windows
+  //     '--disable-gpu'
+  //   ],
+  // },
   authStrategy: new LocalAuth()
 });
 
@@ -38,59 +39,11 @@ const client = new Client({
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: __dirname })
-})
-
+});
 
 client.on('authenticated', () => {
   console.log('AUTHENTICATED');
 });
-
-const db = require('./src/config/db');
-const { response } = require('express');
-
-client.on('message', async msg => {
-  const keyword = msg.body.toLowerCase();
-  const replyMessage = await db.getReply(keyword);
-
-  if (replyMessage !== false ) {
-    msg.reply(replyMessage);
-
-  } else if (msg.body == 'Tes') {
-    msg.sendMessage(msg.from, "Hey There !! Whatsapp Bot under Developmnet");
-  }
-});
-
-// //Send Message
-// app.post('/send-message', [
-//   body('number').notEmpty,
-//   body('message').notEmpty,
-// ], (req, res) =>{
-//   const error = validationResult(req).formatWith(({}) => {
-//     return msg;
-//   });
-
-//   if (!error.isEmpty()){
-//     return res.status(422).json({
-//       status: false,
-//       message: errors.mapped()
-//     })
-//   }
-
-//   const number = req.body.number;
-//   const message = req.body.message;
-
-//   client.sendMessage(number, message).then(response => {
-//     res.status(200).json({
-//       status: true,
-//       response: response
-//     });
-//   }).catch(err => {
-//     res.status(500).json({
-//       status: false,
-//       response: err,
-//     });
-//   });
-// });
 
 client.initialize();
 
@@ -113,6 +66,56 @@ io.on('connection', function(socket){
   })
   
 })
+
+
+const media =  MessageMedia.fromFilePath('./media/kapal.png')
+const db = require('./src/config/db');
+
+
+client.on('message', async msg => {
+  const keyword = msg.body.toLowerCase();
+  const replyMessage = await db.getReply(keyword);
+
+  if (replyMessage !== false ) {
+    msg.reply(replyMessage);
+
+  } else if (msg.body == 'Tes') {
+    msg.reply( "Hey There !! Whatsapp Bot under Developmnet");
+    client.sendSeen();
+  } else if (msg.body === 'cek'){
+    client.sendMessage(msg.from, "Hey There !! Whatsapp Bot under Developmnet"),
+    client.sendSeen();
+  } else if (msg.body.startsWith('sendto ')){
+    let number =  msg.body.split(' ')[1];
+    let messageIndex =  msg.body.slice(messageIndex, msg.body.length);
+    number =  number.includes('@c.us') ? number : '${number}@c.us';
+    let chat = await msg.getChat();
+    chat.sendMessage();
+    client.searchMessages(number, message);
+  } else if (msg.body == '!media') {
+    client.sendMessage(msg.from, media);
+  }
+});
+
+// const {kontak, pesan} = kontak
+
+app.post('/send-message',  (req, res) => {
+  let number = req.body.number;
+  let message = req.body.message;
+
+  client.sendMessage(number, message).then(response => {
+    res.status(200).json({
+      status: true,
+      response: response
+    });
+  }).catch(err => {
+    res.status(500).json({
+      status: false,
+      response: err,
+    });
+  });
+});
+
 server.listen(3000, function () {
   console.log('Running on port 3000')
 }) 
