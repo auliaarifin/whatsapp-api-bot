e38753f3c99348f30625045ca29be89bd7d4ce13
auliaarifin <auliaarifin89@gmail.com>
2022-12-27 08:20:00 +0700

    add list and button but button doesn't send

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia, Buttons, List } = require('whatsapp-web.js');
 const express = require('express');
 const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 const http = require('http');
 const cron = require('node-cron')
-const {body, validationResult } = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const TEST_JID = '6281393233307@c.us';
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 const kontak = require("./contact.json")
 
+const nomor = ['6281393233307@c.us', '"6289505654708@c.us"'];
+const imgUrl = './media/kapal.png'
+
 app.use(express.json());
 app.use(express.urlencoded({
-  extended:true
+  extended: true
 }));
 
 
 const client = new Client({
-  // restartOnAuthFail: true,
-  // puppeteer: {
-  //   headless: true,
-  //   args: [
-  //     '--no-sandbox',
-  //     '--disable-setuid-sandbox',
-  //     '--disable-dev-shm-usage',
-  //     '--disable-accelerated-2d-canvas',
-  //     '--no-first-run',
-  //     '--no-zygote',
-  //     // '--single-process', // <- this one doesn't works in Windows
-  //     '--disable-gpu'
-  //   ],
-  // },
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      // '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
+  },
   authStrategy: new LocalAuth()
 });
 
@@ -49,26 +53,26 @@ client.initialize();
 
 //socket IO
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   socket.emit('message', 'Connecting....')
 
   //barcode
   client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
-    qrcode.toDataURL(qr,(err, url) => {
+    qrcode.toDataURL(qr, (err, url) => {
       socket.emit('qr', url);
       socket.emit('message', 'QR Code Recived, silahkan scan')
     });
   });
 
-  client.on('ready',()=>{
+  client.on('ready', () => {
     socket.emit('message', 'Whatsapp is ready!')
   })
-  
+
 })
 
 
-const media =  MessageMedia.fromFilePath('./media/kapal.png')
+const media = MessageMedia.fromFilePath('./media/kapal.png')
 const db = require('./src/config/db');
 
 
@@ -76,30 +80,63 @@ client.on('message', async msg => {
   const keyword = msg.body.toLowerCase();
   const replyMessage = await db.getReply(keyword);
 
-  if (replyMessage !== false ) {
+
+
+
+
+  if (replyMessage !== false) {
     msg.reply(replyMessage);
 
   } else if (msg.body == 'Tes') {
-    msg.reply( "Hey There !! Whatsapp Bot under Developmnet");
+    msg.reply("Hey There !! Whatsapp Bot under Developmnet");
     client.sendSeen();
-  } else if (msg.body === 'cek'){
+  } else if (msg.body === 'cek') {
     client.sendMessage(msg.from, "Hey There !! Whatsapp Bot under Developmnet"),
-    client.sendSeen();
-  } else if (msg.body.startsWith('sendto ')){
-    let number =  msg.body.split(' ')[1];
-    let messageIndex =  msg.body.slice(messageIndex, msg.body.length);
-    number =  number.includes('@c.us') ? number : '${number}@c.us';
+      client.sendSeen();
+  } else if (msg.body.startsWith('!sendto')) {
+    let number = msg.body.split(' ')[1];
+    let messageIndex = msg.body.slice(messageIndex, msg.body.length);
+    number = number.includes('@c.us') ? number : `${number}@c.us`;
     let chat = await msg.getChat();
     chat.sendMessage();
     client.searchMessages(number, message);
   } else if (msg.body == '!media') {
-    client.sendMessage(msg.from, media);
+    client.sendMessage(msg.from, media, { caption: `'Here\n your requested media.` });
+  } else if (msg.body === '!buttons') {
+    let button = new Buttons(
+      'Button body',
+      [
+        { body: 'whatsapp-web.js', url: 'https://wwebjs.dev/' },
+        { body: 'Call me', number: '+62 (805) 457-4992' },
+        { body: 'third special button', number: '+1 (202) 968-6161' },// Limited to 2 especial buttons, this one will be ignored
+        { body: 'Some text' },
+        { body: 'Another text' },
+        { body: 'Another another text' },
+        { body: 'Fourth button' }// Limited to 3 regular buttons, this one will be ignored
+      ],
+      'title',
+      'footer'
+    );
+
+    client.sendMessage(msg.from, button);
+    console.log('message','terkirim');
+  } else if (msg.body === '!list') {
+    let sections = [{
+
+      title: 'Selamat Datang Lorem Ipsum',
+      rows: [
+        { title: 'Judul 2', description: 'Cara memakai Whatsapp API' },
+        { title: 'Judul 3', description: 'Cara memakai Whatsapp API' }
+      ]
+    }];
+    let list = new List('Whatsaapp', 'Button Tombol', sections, 'Title', 'footer');
+    client.sendMessage(msg.from, list);
   }
 });
 
 // const {kontak, pesan} = kontak
 
-app.post('/send-message',  (req, res) => {
+app.post('/send-message', (req, res) => {
   let number = req.body.number;
   let message = req.body.message;
 
@@ -118,7 +155,7 @@ app.post('/send-message',  (req, res) => {
 
 server.listen(3000, function () {
   console.log('Running on port 3000')
-}) 
+})
 
 // cron.schedule('* * * * * *', () =>{
 //   console.log('Testing scheduler')
